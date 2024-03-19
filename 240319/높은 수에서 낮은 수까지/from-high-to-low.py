4ea887bf593680680055095760ee425eb52507dd
jiyeon0623 <139705694+jiyeon0623@users.noreply.github.com>
2024-03-19 09:41:45 +0900

    Title: 높은 수에서 낮은 수까지, Time: 137ms, Memory: 24MB, Status: Passed - CodeTree

--- 240319/높은 수에서 낮은 수까지/from-high-to-low.py
+++ 240319/높은 수에서 낮은 수까지/from-high-to-low.py
@@ -3,7 +3,7 @@ a,b = int(a), int(b)
 
 if b>=a:
     for i in range(b,a-1, -1):
-        print(b, end = " ")
+        print(i, end = " ")
 else:
     for i in range(a,b-1,-1):
-        print(a, end = " ")
+        print(i, end = " ")
