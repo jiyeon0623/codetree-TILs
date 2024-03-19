4a5348e5fb1c7234b8c42b6f279db1e5024cfb22
jiyeon0623 <139705694+jiyeon0623@users.noreply.github.com>
2024-03-19 16:57:21 +0900

    Title: 살 수 있는 물건 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240319/살 수 있는 물건 2/things-able-to-buy-2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    scanf("%d", a);
+
+    if (a>=3000){
+        printf("book");
+    }
+    else if ( a>=1000){
+        printf("mask");
+    }
+    else if (a>=500){
+        printf("pen")
+    }
+    else {
+        printf("no")
+    }
+    return 0;
+}
