0961b8cdc6c1ed3e832d6f38db56214d6c0af26f
jiyeon0623 <139705694+jiyeon0623@users.noreply.github.com>
2024-03-19 16:37:07 +0900

    Title: 시력 검사 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240319/시력 검사 2/eye-test-2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    float a;
+    scanf("%.1f", &a);
+    if (a >=1.0){
+        printf("High");
+    }
+    else if (a>=0.5){
+        printf("Middle");
+    }
+    else {
+        printf("Low");
+    }
+    return 0;
+}
