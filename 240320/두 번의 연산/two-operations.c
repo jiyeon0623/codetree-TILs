a71894e53bd2857dc56e3d60d3d880bed5fcb7d7
jiyeon0623 <139705694+jiyeon0623@users.noreply.github.com>
2024-03-20 13:16:30 +0900

    Title: 두 번의 연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240320/두 번의 연산/two-operations.c
+++ 240320/두 번의 연산/two-operations.c
@@ -7,10 +7,12 @@ int main() {
     
     if (a %2 ==1){
         a+=3;
-        if (a%3 == 0){
-            a/=3;
+        
+    }
 
-        }
+    if (a%3 == 0){
+        a/=3;
+
     }
     printf("%d", a);
     return 0;
