8a78461fd3e4411fa0efcbddb6d450a5f7f7f90f
jiyeon0623 <139705694+jiyeon0623@users.noreply.github.com>
2024-03-20 13:52:00 +0900

    Title: 4번의 크기 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240320/4번의 크기 비교/4-time-comparison.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b,c,d,e;
+    scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
+
+    if (a>b){
+        printf("%d\n", 1);
+    }
+    else{
+        printf("%d\n", 0);
+    }
+    if (a>c){
+        printf("%d\n", 1);
+    }
+    else{
+        printf("%d\n", 0);
+    }
+    if (a>d){
+        printf("%d\n", 1);
+    }
+    else{
+        printf("%d\n", 0);
+    }
+    if (a>e){
+        printf("%d\n", 1);
+    }
+    else{
+        printf("%d\n", 0);
+    }
+    return 0;
+}
