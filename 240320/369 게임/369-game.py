9fabe4bbda079214923a310fdc8162cf1858ff83
jiyeon0623 <139705694+jiyeon0623@users.noreply.github.com>
2024-03-20 14:47:42 +0900

    Title: 369 게임, Time: 175ms, Memory: 24MB, Status: Passed - CodeTree

--- 240320/369 게임/369-game.py
+++ 240320/369 게임/369-game.py
@@ -1,7 +1,8 @@
 n = int(input())
 
 for i in range(1,n+1):
-    if (i %3 == 0 or str(i) in ['3','6','9']):
+    if (i %3 == 0 or i //10 in [3,6,9] or i %10 in [3,6,9]):
         print(0, end = " ")
+    
     else:
         print(i, end = " ")
