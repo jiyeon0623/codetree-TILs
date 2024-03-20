6b0e3401db17e222f506cf58579bf6162fc83c05
jiyeon0623 <139705694+jiyeon0623@users.noreply.github.com>
2024-03-20 13:11:43 +0900

    Title: 최대 2번의 연산, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240320/최대 2번의 연산/up-to-2-calculations.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    scanf("%d", &a);
+
+    if (a %2 == 0){
+        a/=2;
+
+    }
+    else{
+        a+=1;
+        a/=2;
+    }
+    printf("%d", a);
+    return 0;
+}
