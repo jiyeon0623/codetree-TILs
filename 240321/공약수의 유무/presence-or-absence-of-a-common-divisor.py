5d4e6162459c072ee6fa133d03d1239e4fca2b0d
jiyeon0623 <139705694+jiyeon0623@users.noreply.github.com>
2024-03-21 22:48:18 +0900

    Title: 공약수의 유무, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

--- 240321/공약수의 유무/presence-or-absence-of-a-common-divisor.py
+++ 240321/공약수의 유무/presence-or-absence-of-a-common-divisor.py
@@ -2,7 +2,7 @@ a,b = input().split()
 a,b = int(a), int(b)
 bool1= False
 for i in range(a,b+1):
-    if (1920 % a == 0 and 2880 %a == 0):
+    if (1920 % i == 0 and 2880 %i == 0):
         bool1 = True
     
 if bool1:
