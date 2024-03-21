e65771e8c81eb0f7c165f91b26f76fd8d2c85df2
jiyeon0623 <139705694+jiyeon0623@users.noreply.github.com>
2024-03-21 23:23:03 +0900

    Title: 최댓값 구하기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240321/최댓값 구하기/maximum-value.c
+++ 240321/최댓값 구하기/maximum-value.c
@@ -4,14 +4,23 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int a,b,c;
     scanf("%d %d %d", &a, &b, &c);
-    if (a>=b && b>=c){
-        printf("%d", a);
+    if (a>=b ){
+        if (a>=c){
+            printf("%d", a);
+        }
+        else{
+            printf("%d", c);
+        }
+        
     }
-    else if (b>=a && b>=c){
-        printf("%d", b);
-    }
-    else{
-        printf("%d", c);
+    
+    else{ //b가 a보다 큰 경우
+        if (b>=c){
+            printf("%d", b);
+        }
+        else{
+            printf("%d", c);
+        }
     }
     return 0;
 }
