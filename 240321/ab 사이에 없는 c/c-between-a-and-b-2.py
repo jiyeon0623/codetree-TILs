38acabcb0a6cdaf34089c153949712dd5a64d4ca
jiyeon0623 <139705694+jiyeon0623@users.noreply.github.com>
2024-03-21 22:55:42 +0900

    Title: ab 사이에 없는 c, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240321/ab 사이에 없는 c/c-between-a-and-b-2.py
+++ 240321/ab 사이에 없는 c/c-between-a-and-b-2.py
@@ -4,7 +4,8 @@ satisfied = True
 for i in range(a,b+1):
     if i % c != 0:
         satisfied = False
-if not satisfied:
-    print("YES")
-else:
+
+if satisfied == True:
     print("NO")
+else:
+    print("YES")
