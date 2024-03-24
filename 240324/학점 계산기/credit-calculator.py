05396c6333721f3f7e88c200bdc3e3665dbf88b2
jiyeon0623 <139705694+jiyeon0623@users.noreply.github.com>
2024-03-24 20:20:10 +0900

    Title: 학점 계산기, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240324/학점 계산기/credit-calculator.py
@@ -0,0 +1,13 @@
+n = int(input())
+arr = map(float, input().split())
+
+grade = sum(arr)/n
+
+print(f"{grade:.1f}")
+
+if grade >=4.0:
+    print("{Perfect")
+elif grade >= 3.0:
+    print("Good")
+else:
+    print("Poort")
