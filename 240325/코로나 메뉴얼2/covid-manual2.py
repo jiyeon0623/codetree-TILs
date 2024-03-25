8c05233d271839cb509ba35dadaf4f0a60f24a83
jiyeon0623 <139705694+jiyeon0623@users.noreply.github.com>
2024-03-25 12:05:44 +0900

    Title: 코로나 메뉴얼2, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240325/코로나 메뉴얼2/covid-manual2.py
@@ -0,0 +1,20 @@
+new_arr = [0] * 4
+for i in range(3):
+    arr = input().split()
+    
+    if arr[0] == 'Y':
+        if int(arr[1]) >=37:
+            new_arr[0]+=1
+        else:
+            new_arr[2]+=1
+    else:
+        if int(arr[1]) >=37:
+            new_arr[1]+=1
+        else:
+            new_arr[3]+=1
+
+if sum(new_arr)>=3:
+    new_arr.append("E")
+
+for elem in new_arr:
+    print(elem, end = " ")
